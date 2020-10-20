bcc1f8e23210d3614ee9185b2c8062e266df3762
Celso Bertolotto <celsonyl@hotmail.com>
2020-10-20 15:15:13 -0300

    Começando a implementar o tab

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -7,11 +7,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao p;
 
-            p = new Posicao(3, 4);
-
-            Console.WriteLine(p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.WriteLine(tab);
         }
     }
 }
---
+++ xadrez-console/Tabuleiro/Cor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    enum Cor{
+        Branca,
+        Preta,
+
+
+
+
+        //
+        Amarela,
+        Azul,
+        Vermelha
+
+    }
+}
