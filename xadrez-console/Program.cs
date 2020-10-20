5d0493e1981cd6b8a8b01cc07369f42db2fe2797
Celso Bertolotto <celsonyl@hotmail.com>
2020-10-20 14:58:03 -0300

    Classe Posição criada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
         }
     }
 }
