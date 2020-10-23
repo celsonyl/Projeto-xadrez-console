e71f0fa6c93684c1be234d159754da1d5d7ab3de
Celso Bertolotto <celsonyl@hotmail.com>
2020-10-23 14:09:53 -0300

    Fazendo movimentacao das pessas. APENAS

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -11,12 +11,27 @@ namespace xadrez_console
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
-                
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
 
 
-            }catch(TabuleiroException e)
+                }
+
+
+
+            }
+            catch(TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }       
