db968c8bbd83ade5da6002b56307b45e9a97b7f9
Celso Bertolotto <celsonyl@hotmail.com>
2020-10-29 14:44:33 -0300

    att o metodo realiza movimento e desfaz movimento com a jogada passant

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -63,6 +63,27 @@ namespace xadrez
                 tab.colocarPeca(T, destinoTorre);
             }
 
+            //JOGADA PASSANT
+            if(p is Peao)
+            {
+                if(origem.coluna != destino.coluna && pecaCapturada == null)
+                {
+                    Posicao posPeao;
+                    if(p.cor == Cor.Branca)
+                    {
+                        posPeao = new Posicao(destino.linha + 1, destino.coluna);
+
+                    }
+                    else
+                    {
+                        posPeao = new Posicao(destino.linha - 1, destino.coluna);
+
+                    }
+                    pecaCapturada = tab.retirarPeca(posPeao);
+                    capturadas.Add(pecaCapturada);
+                }
+            }
+
             return pecaCapturada;
         }
 
@@ -95,6 +116,25 @@ namespace xadrez
                 Peca T = tab.retirarPeca(destinoTorre);
                 T.decrementarQuantidadeMovimento();
                 tab.colocarPeca(T, origemTorre);
+            }
+
+            //JOGADA PASSANT
+            if(p is Peao)
+            {
+                if(origem.coluna != destino.coluna && pecaCapturada == vulneravelPassant)
+                {
+                    Peca peao = tab.retirarPeca(destino);
+                    Posicao posPeao;
+                    if(p.cor == Cor.Branca)
+                    {
+                        posPeao = new Posicao(3, destino.coluna);
+                    }
+                    else
+                    {
+                        posPeao = new Posicao(4, destino.coluna);
+                    }
+                    tab.colocarPeca(peao, posPeao);
+                }
             }
         }
 
@@ -299,14 +339,14 @@ namespace xadrez
             colocarNovaPeca('g', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('h', 1, new Torre(tab, Cor.Branca));
 
-            colocarNovaPeca('a', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('b', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('c', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('d', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('e', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('f', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('g', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('h', 2, new Peao(tab, Cor.Branca));
+            colocarNovaPeca('a', 2, new Peao(tab, Cor.Branca,this));
+            colocarNovaPeca('b', 2, new Peao(tab, Cor.Branca,this));
+            colocarNovaPeca('c', 2, new Peao(tab, Cor.Branca,this));
+            colocarNovaPeca('d', 2, new Peao(tab, Cor.Branca,this));
+            colocarNovaPeca('e', 2, new Peao(tab, Cor.Branca,this));
+            colocarNovaPeca('f', 2, new Peao(tab, Cor.Branca,this));
+            colocarNovaPeca('g', 2, new Peao(tab, Cor.Branca,this));
+            colocarNovaPeca('h', 2, new Peao(tab, Cor.Branca,this));
 
 
 
@@ -322,14 +362,14 @@ namespace xadrez
             colocarNovaPeca('g', 8, new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('h', 8, new Torre(tab, Cor.Preta));
 
-            colocarNovaPeca('a', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('b', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('c', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('d', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('e', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('f', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('g', 7, new Peao(tab, Cor.Preta));
-            colocarNovaPeca('h', 7, new Peao(tab, Cor.Preta));
+            colocarNovaPeca('a', 7, new Peao(tab, Cor.Preta,this));
+            colocarNovaPeca('b', 7, new Peao(tab, Cor.Preta,this));
+            colocarNovaPeca('c', 7, new Peao(tab, Cor.Preta,this));
+            colocarNovaPeca('d', 7, new Peao(tab, Cor.Preta,this));
+            colocarNovaPeca('e', 7, new Peao(tab, Cor.Preta,this));
+            colocarNovaPeca('f', 7, new Peao(tab, Cor.Preta,this));
+            colocarNovaPeca('g', 7, new Peao(tab, Cor.Preta,this));
+            colocarNovaPeca('h', 7, new Peao(tab, Cor.Preta,this));
 
 
         }
